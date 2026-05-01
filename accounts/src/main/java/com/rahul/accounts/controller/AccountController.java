c31ac475644b22fc6f13cb53ba4b06ad47d7d34d
rahulmulik9 <rahulmulik9@gmail.com>
2026-05-01 10:50:16 +0530

    Added request Mapping

--- accounts/src/main/java/com/rahul/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/rahul/accounts/controller/AccountController.java
@@ -1,9 +1,13 @@
 package com.rahul.accounts.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountController {
 
     @GetMapping("sayHello")
