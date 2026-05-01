f924325c47a51d39c0f49ab2effc98b1ad0c4943
rahulmulik9 <rahulmulik9@gmail.com>
2026-05-01 14:03:11 +0530

    Added api to update customer info

--- accounts/src/main/java/com/rahul/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/rahul/accounts/controller/AccountController.java
@@ -39,4 +39,19 @@ public class AccountController {
         CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
+
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
+        boolean isUpdated = iAccountsService.updateAccount(customerDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
+        }
+    }
 }
--- accounts/src/main/java/com/rahul/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/rahul/accounts/service/IAccountsService.java
@@ -7,5 +7,5 @@ public interface IAccountsService {
 
     void createAccount(CustomerDto customerDto);
     CustomerDto fetchAccount(String mobileNumber);
-
+    boolean updateAccount(CustomerDto customerDto);
 }
