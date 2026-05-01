6dd702e2458634b53a2ee8c972847e118c602e1f
rahulmulik9 <rahulmulik9@gmail.com>
2026-05-01 16:15:26 +0530

    Added comments

--- accounts/src/main/java/com/rahul/accounts/entity/BaseEntity.java
+++ accounts/src/main/java/com/rahul/accounts/entity/BaseEntity.java
@@ -20,6 +20,17 @@ import java.time.LocalDateTime;
 @ToString
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+  /* @Column(updatable = false)
+     It tells JPA to exclude that column from the SQL UPDATE statement. The column is only written during INSERT.
+     Used on createdAt and createdBy so they are set once and never changed.
+
+     @Column(insertable = false)
+     It tells JPA to exclude that column from the SQL INSERT statement.
+     The column is only written during UPDATE. Used on updatedAt and updatedBy so they are only set when a record is modified.
+
+    @CreatedDate / @LastModifiedDate → automatically fills timestamp (Spring handles this itself, no extra setup needed)
+    @CreatedBy / @LastModifiedBy → automatically fills who did it, but Spring doesn't know who the current user is on its own — so it asks your AuditorAware<String> bean for that value.
+    */
 
     @CreatedDate
     @Column(updatable = false)
