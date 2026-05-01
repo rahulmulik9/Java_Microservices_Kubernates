a710cb6777e1c25982e3727cabefec8fecead5bb
rahulmulik9 <rahulmulik9@gmail.com>
2026-05-01 10:37:20 +0530

    Added accountdto

---
+++ accounts/src/main/java/com/rahul/accounts/dto/AccountsDto.java
@@ -0,0 +1,15 @@
+package com.rahul.accounts.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public class AccountsDto {
+
+    @NotEmpty(message = "AccountNumber can not be a null or empty")
+    private Long accountNumber;
+
+    @NotEmpty(message = "AccountType can not be a null or empty")
+    private String accountType;
+
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
+    private String branchAddress;
+}
