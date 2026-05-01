8fa76ef2a06b8c408829dfc1599544d3a6814cb1
rahulmulik9 <rahulmulik9@gmail.com>
2026-05-01 10:19:56 +0530

    Lombok annotation

--- accounts/src/main/java/com/rahul/accounts/entity/BaseEntity.java
+++ accounts/src/main/java/com/rahul/accounts/entity/BaseEntity.java
@@ -1,8 +1,14 @@
 package com.rahul.accounts.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@ToString
 public class BaseEntity {
 
     private LocalDateTime createdAt;
