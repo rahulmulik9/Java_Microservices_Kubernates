3ce09de4813cf7ad5c54693356d3565565fbf920
rahulmulik9 <rahulmulik9@gmail.com>
2026-05-01 15:27:37 +0530

    Validate annotation in Controller

--- accounts/src/main/java/com/rahul/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/rahul/accounts/controller/AccountController.java
@@ -10,11 +10,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@Validated
 @AllArgsConstructor
 public class AccountController {
     private IAccountsService iAccountsService;
