af017b37cd70976e89427435b0cdd0da2c97bc66
rahulmulik9 <rahulmulik9@gmail.com>
2026-05-01 10:18:30 +0530

    Added Entity class

---
+++ accounts/src/main/java/com/rahul/accounts/entity/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.rahul.accounts.entity;
+
+
+import java.time.LocalDateTime;
+
+public class BaseEntity {
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+}
