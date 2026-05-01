6ffa6c682e5ecc3aaec6c67819d7a1e9c018aec9
rahulmulik9 <rahulmulik9@gmail.com>
2026-05-01 14:06:28 +0530

    Added api to Delete account and customer

--- accounts/src/main/java/com/rahul/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/rahul/accounts/service/IAccountsService.java
@@ -8,4 +8,5 @@ public interface IAccountsService {
     void createAccount(CustomerDto customerDto);
     CustomerDto fetchAccount(String mobileNumber);
     boolean updateAccount(CustomerDto customerDto);
+    boolean deleteAccount(String mobileNumber);
 }
