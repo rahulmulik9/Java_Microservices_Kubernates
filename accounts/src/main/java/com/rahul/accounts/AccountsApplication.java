3eab4e3471f064eaf224476865b83c804a677239
rahulmulik9 <rahulmulik9@gmail.com>
2026-05-02 13:48:03 +0530

    Added API to fetch Details     @Value("${build.version}")     private String buildVersion;     this fetch from yml file
       and if class is annotated by
       @ConfigurationProperties(prefix = "name_property")
    
       then spring create json to this class object

--- accounts/src/main/java/com/rahul/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/rahul/accounts/AccountsApplication.java
@@ -1,15 +1,18 @@
 package com.rahul.accounts;
 
+import com.rahul.accounts.dto.AccountsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})    //Register this class as a bean so it can be injected
 @OpenAPIDefinition(
         info = @Info(
                 title = "Accounts microservice REST API Documentation",
