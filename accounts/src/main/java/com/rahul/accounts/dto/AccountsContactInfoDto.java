ddb854bb534e7eba7d44bd62e7b70adc2dfa8863
Rahul Mulik <rahul.mulik@prototechsolutions.com>
2026-05-07 12:57:25 +0530

    Removed record and added class

--- accounts/src/main/java/com/rahul/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/com/rahul/accounts/dto/AccountsContactInfoDto.java
@@ -2,13 +2,26 @@ package com.rahul.accounts.dto;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
 import java.util.List;
 import java.util.Map;
 
 
 /*@ConfigurationProperties : Marks your class as a holder for configuration data
 Bind everything under accounts in YAML to this class*/
+//@ConfigurationProperties(prefix = "accounts")
+//public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+//
+//}
+
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 
 }
--- cards/src/main/java/com/rahul/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/rahul/cards/dto/CardsContactInfoDto.java
@@ -1,10 +1,22 @@
 package com.rahul.cards.dto;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-
+import lombok.Getter;
+import lombok.Setter;
 import java.util.List;
 import java.util.Map;
 
+//@ConfigurationProperties(prefix = "cards")
+//public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+//}
+
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 }
