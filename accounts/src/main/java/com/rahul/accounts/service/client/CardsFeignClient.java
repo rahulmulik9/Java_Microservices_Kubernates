3c145109202b95a6a6ef59ff523ba20b4384d7ae
Rahul Mulik <rahul.mulik@prototechsolutions.com>
2026-05-20 15:31:13 +0530

    Added feign client class

--- accounts/src/main/java/com/rahul/accounts/service/client/CardsFeignClient.java
+++ accounts/src/main/java/com/rahul/accounts/service/client/CardsFeignClient.java
@@ -1,5 +1,6 @@
 package com.rahul.accounts.service.client;
 
+import com.rahul.accounts.dto.CardsDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
