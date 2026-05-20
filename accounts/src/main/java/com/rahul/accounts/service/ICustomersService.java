b2d828cbe0645684d1b37c4513a7819f8bd484a6
Rahul Mulik <rahul.mulik@prototechsolutions.com>
2026-05-20 15:47:54 +0530

    Added Service for Customer

---
+++ accounts/src/main/java/com/rahul/accounts/service/ICustomersService.java
@@ -0,0 +1,13 @@
+package com.rahul.accounts.service;
+
+import com.rahul.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomersService {
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+}
